f3f5ff04b93b845da98512fc2e148fd20f93f351
Guy Bachkhaznadji <guysergebach@hotmail.com>
2019-03-31 17:11:45 +0100

    fixed db path problem

--- db/config.py
+++ db/config.py
@@ -1,13 +1,12 @@
 from configparser import ConfigParser
-  
-def config(filename='db/database.ini', section='cats'):
+
+
+def config(filename='db/database.ini', section='postgresql'):
     # create a parser
     configParser = ConfigParser()
     # read config file
     configParser.read(filename)
-    
-    # params = configParser.get(section)
-    print(configParser.read(filename))
+
     # get section, default to postgresql
     db = {}
     if configParser.has_section(section):
@@ -15,6 +14,7 @@ def config(filename='db/database.ini', section='cats'):
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
- 
+        raise Exception(
+            'Section {0} not found in the {1} file'.format(section, filename))
+
     return db
